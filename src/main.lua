3859d3e09130152ad07f9afde3212f9918adeace
wangyu <365428695@qq.com>
2019-12-22 01:49:30 +0800

    fix:引入luaide调试代码 & 处理simulator没有控制台的问题

--- src/main.lua
+++ src/main.lua
@@ -1,5 +1,17 @@
 
+--@region luaide调试代码
+local breakSocketHandle,debugXpCall = require("LuaDebugjit")("localhost",7004)
+cc.Director:getInstance():getScheduler():scheduleScriptFunc(breakSocketHandle, 0.3, false)
+--@endregion
+
 cc.FileUtils:getInstance():setPopupNotify(false)
+
+__G__TRACKBACK__ = function(msg)
+    local msg = debug.traceback(msg, 3)
+    print(msg)
+    debugXpCall()
+    return msg
+end
 
 require "config"
 require "cocos.init"
